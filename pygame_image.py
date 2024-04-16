b6a3ee5e8200dd9a1ecd0a1869b09730d044c696
SATORU SHIBUYA <c0a230784c@edu.teu.ac.jp>
2024-04-16 17:26:18 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -13,19 +13,30 @@ def main():
     bg_img2= pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("fig/3.png")#練習2
     kk_img = pg.transform.flip(kk_img,True,False)#反転
+    kk_rct = kk_img.get_rect()#練習8-1
+    kk_rct.center=300,200
     tmr = 0
     bg_x = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_list = pg.key.get_pressed()
+        if key_list[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_list[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_list[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_list[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
 
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])#練習6
         screen.blit(bg_img2,[-x+1600,0])#練習7-1
         screen.blit(bg_img, [-x+3200, 0])#練習6
         screen.blit(bg_img2,[-x+4800,0])#練習7-2
-        screen.blit(kk_img,[300,200])#練習7-2
+        screen.blit(kk_img,kk_rct.center)#練習7-2
         pg.display.update()
         tmr += 1        
         clock.tick(200)#練習5
