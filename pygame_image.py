3d8be975876236be939952009ab7b8ada8da3f65
SATORU SHIBUYA <c0a230784c@edu.teu.ac.jp>
2024-04-16 17:45:09 +0900

    演習1,2（修正）

--- pygame_image.py
+++ pygame_image.py
@@ -15,21 +15,24 @@ def main():
     kk_img = pg.transform.flip(kk_img,True,False)#反転
     kk_rct = kk_img.get_rect()#練習8-1
     kk_rct.center=300,200
+    kk_dx=0
+    kk_dy=0
     tmr = 0
     kk_x = 0
 
     while True:
+        
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_list = pg.key.get_pressed()
         if key_list[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            kk_dy=-1
         if key_list[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
+            kk_dy=1
         if key_list[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            kk_dx=-1
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip((1,0))
+            kk_dx=2
 
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])#練習6
@@ -39,7 +42,9 @@ def main():
         screen.blit(kk_img,kk_rct.center)#練習7-2
         pg.display.update()
         tmr += 1   
-        kk_rct.move_ip((-1,0))
+        kk_rct.move_ip((-1+kk_dx,kk_dy))
+        kk_dx=0
+        kk_dy=0
         clock.tick(200)#練習5
 
 
